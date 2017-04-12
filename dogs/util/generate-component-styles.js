fb7a725743466d29206aa59e53681ce198f74d5c
Piet Geursen <pietgeursen@gmail.com>
2017-04-12 12:25:02 +1200

    Refactor

--- dogs/util/generate-component-styles.js
+++ dogs/util/generate-component-styles.js
@@ -1,9 +1,10 @@
-import { map } from 'ramda'
+import { map, __ } from 'ramda'
 
 export default function generateComponentStyles (styles) {
+  const mapWithStyles = map(__, styles)
   return (props) => (renderer) => {
-    return map((style) => {
+    return mapWithStyles((style) => {
       return renderer.renderRule(style, props)
-    }, styles)
+    })
   }
 }
