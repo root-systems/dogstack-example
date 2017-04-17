913a808d7ca07d054a659c5d9114b4c51dffd003
Michael Williams <michael.williams@enspiral.com>
2017-04-17 20:08:09 +1200

    extract and use `uify-server`, add pino logs

---
+++ log.js
@@ -0,0 +1,19 @@
+const logDebug = require('pino-debug')
+const Log = require('pino')
+const prettyStream = require('pino-colada')
+
+const level = process.env.LEVEL || 'info'
+const pretty = process.env.NODE_ENV === 'development'
+const name = 'example'
+
+var stream
+if (pretty) {
+  stream = prettyStream()
+  stream.pipe(process.stdout)
+}
+
+const log = Log({ name, level }, stream)
+
+logDebug(log)
+
+module.exports = log
