ff76725f4b17bf75936e71786c7edc10efe0b535
Mikey <michael.williams@enspiral.com>
2017-05-04 10:56:33 +1200

    extract some boilerplate into dogstack cli 0.1 (#20)
    * extract some boilerplate into dogstack cli 0.1
    
    * publish dogstack@0.1

--- db/index.js
+++ db/index.js
@@ -1,4 +1,65 @@
-const Knex = require('knex')
-const config = require('./knexfile')[process.env.NODE_ENV || 'development']
+var { join } = require('path')
+// Update with your config settings.
 
-module.exports = Knex(config)
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: join(__dirname, 'dev.sqlite3')
+    },
+    migrations: {
+      directory: join(__dirname, 'migrations')
+    },
+    seeds: {
+      directory: join(__dirname, 'seeds')
+    }
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:'
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
+
+}
