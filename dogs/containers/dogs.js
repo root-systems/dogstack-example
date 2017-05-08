7f55fe10a3b3311208328a883c1aa3647532fd85
Michael Williams <michael.williams@enspiral.com>
2017-05-08 15:41:30 +1200

    a real module now

--- dogs/containers/dogs.js
+++ dogs/containers/dogs.js
@@ -1,4 +1,4 @@
-import { connectFeathers } from '../../lib/feathers-action-react'
+import { connect } from 'feathers-action-react'
 
 import Dogs from '../components/dogs'
 
@@ -6,11 +6,11 @@ import { actions as dogActions } from '../'
 
 import { getIndexProps } from '../getters'
 
-export default connectFeathers({
+export default connect({
   selector: getIndexProps,
   actions: { dogs: dogActions },
-  query: (props) => [{
-    resource: 'dogs',
+  query: {
+    service: 'dogs',
     params: {}
-  }]
+  }
 })(Dogs)
