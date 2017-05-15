23854cd4e3568a617de3397380990c614b0c752d
Michael Williams <michael.williams@enspiral.com>
2017-05-15 17:24:46 +1200

    only use redux-logger in dev env

--- store.js
+++ store.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { createEpicMiddleware } from 'redux-observable'
-import { createLogger } from 'redux-logger'
 import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
 
 import feathers from './client'
@@ -10,9 +9,13 @@ import rootEpic from './epic'
 export default function configureStore ({ history }) {
   const middleware = [
     createEpicMiddleware(rootEpic, { dependencies: { feathers } }),
-    routerMiddleware(history),
-    createLogger()
+    routerMiddleware(history)
   ]
+
+  if (process.env.NODE_ENV === 'development') {
+    const { logger } = require(`redux-logger`)
+    middleware.push(logger)
+  }
 
   const enhancer = compose(
     applyMiddleware(...middleware)
