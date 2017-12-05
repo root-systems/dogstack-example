819f205c39ff158261f22f281249a8873dc5001f
Michael Smith <michael@msmith.nz>
2017-12-05 12:09:19 +0000

    complete step one

---
+++ dogs/components/Dog.js
@@ -0,0 +1,9 @@
+import h from 'react-hyperscript'
+
+const Dog = (props) => {
+  return (
+    h('p', {}, 'Woof!')
+  )
+}
+
+export default Dog
--- routes.js
+++ routes.js
@@ -1,5 +1,17 @@
 import { Route } from 'react-router-dom'
 import React from 'react'
 
+import Dog from './dogs/components/Dog'
+
 export default [
+  {
+    name: 'dog',
+    path: '/',
+    exact: true,
+    Component: Dog,
+    navigation: {
+      title: 'dogs.dog',
+      icon: 'fa fa-paw'
+    }
+  }
 ]
